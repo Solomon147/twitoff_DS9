79c703b75dcdd7b3c8767a4aca7dc125f1466aef
Hot Stuff <hotstuff@Hots-MacBook.local>
2019-12-17 17:45:17 -0800

    adding the html templates

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -8,7 +8,7 @@ class User(DB.Model):
     name = DB.Column(DB.String(15), nullable=False)
     newest_tweet_id = DB.Column(DB.BigInteger)
     def __repr__(self):
-        return '<User {}'.format(self.name)
+        return '<User {}>'.format(self.name)
     
     
 class Tweet(DB.Model):
@@ -16,3 +16,5 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(300))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
